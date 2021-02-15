eba7f5a970ca00e703afaa71089084c848e6fbde
Ali Saadati <saadati944@gmail.com>
2021-02-15 13:37:17 +0330

    read settings from settings file

--- src/simple/snake/Main.java
+++ src/simple/snake/Main.java
@@ -5,9 +5,27 @@ public class Main
 
     public static void main(String[] args)
     {
+        System.out.println("before reading file :");
+        System.out.print("width  :   ");
         System.out.println(Settings.worldWidth);
+        System.out.print("height :   ");
         System.out.println(Settings.worldHeight);
+        System.out.print("fps    :   ");
         System.out.println(Settings.frameRate);
+        System.out.print("delay  :   ");
         System.out.println(Settings.foodDelay);
+
+        Settings.readFromFile();
+
+        System.out.println("after reading file :");
+        System.out.print("width  :   ");
+        System.out.println(Settings.worldWidth);
+        System.out.print("height :   ");
+        System.out.println(Settings.worldHeight);
+        System.out.print("fps    :   ");
+        System.out.println(Settings.frameRate);
+        System.out.print("delay  :   ");
+        System.out.println(Settings.foodDelay);
+
     }
 }
--- src/simple/snake/Settings.java
+++ src/simple/snake/Settings.java
@@ -1,12 +1,17 @@
 package simple.snake;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class Settings
 {
     //settings file path
     private static final String settingsPath = "./settings.txt";
 
+
     // size of the world
-    public static int worldWidth = 80, worldHeight =20;
+    public static int worldWidth = 80;
+    public static int worldHeight =20;
 
     //game speed
     public static int frameRate = 30;
@@ -16,4 +21,48 @@ public class Settings
     public static int foodDelay = 2;
 
 
+    /**
+     * read settings from settings file if exists
+     */
+    public static void readFromFile()
+    {
+        File settingsFile;
+        Scanner scanner;
+        try
+        {
+            settingsFile = new File(settingsPath);
+            scanner = new Scanner(settingsFile);
+        }
+        catch(Exception e)
+        {
+            return;
+        }
+        while(scanner.hasNextLine())
+        {
+            String line = scanner.nextLine();
+            if(line.startsWith("#") || line.isBlank())
+                continue;
+            else if (line.startsWith("width "))
+                try
+                {
+                    worldWidth = Integer.parseInt(line.substring(6).strip());
+                }catch (Exception e){continue;}
+            else if (line.startsWith("height "))
+                try
+                {
+                    worldHeight = Integer.parseInt(line.substring(7).strip());
+                }catch (Exception e){continue;}
+            else if (line.startsWith("fps "))
+                try
+                {
+                    frameRate = Integer.parseInt(line.substring(4).strip());
+                }catch (Exception e){continue;}
+            else if (line.startsWith("delay "))
+                try
+                {
+                    foodDelay = Integer.parseInt(line.substring(6).strip());
+                }catch (Exception e){continue;}
+
+        }
+    }
 }
