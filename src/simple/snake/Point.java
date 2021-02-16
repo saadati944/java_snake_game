be2d5d34cf7a29df8364463a78adbf247628d3b2
Ali Saadati <saadati944@gmail.com>
2021-02-16 09:30:39 +0330

    add character field to point class

--- src/simple/snake/Point.java
+++ src/simple/snake/Point.java
@@ -1,13 +1,39 @@
 package simple.snake;
 
+import java.util.Objects;
+
 public class Point
 {
-    public int x;
-    public int y;
+    public int x = 0;
+    public int y = 0;
+
+    public char c = Settings.snakeBody;
 
     public Point(int x, int y)
     {
         this.x = x;
         this.y = y;
+    }
+
+    public Point(int x, int y, char c)
+    {
+        this.x = x;
+        this.y = y;
+        this.c = c;
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return x == point.x && y == point.y && c == point.c;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(x, y, c);
     }
 }
