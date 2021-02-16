983c70e548822689b424ad63293820d7805b8163
Ali Saadati <saadati944@gmail.com>
2021-02-16 16:05:18 +0330

    create a linkedlist class

---
+++ src/simple/snake/LinkedList.java
@@ -0,0 +1,17 @@
+package simple.snake;
+
+public class LinkedList
+{
+    public Object value = null;
+    public LinkedList next = null;
+
+    public LinkedList()
+    {
+
+    }
+    public LinkedList(Object value, LinkedList next)
+    {
+        this.value = value;
+        this.next = next;
+    }
+}
